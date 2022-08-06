616648618bb64d0f0a8fa88b44207f939eb15bed
Abel <abelsena1@hotmail.com>
2022-08-06 15:53:03 -0300

    correcao cors 11

--- server.js
+++ server.js
@@ -46,6 +46,11 @@ app.use("/", require("./routes"));
 
 // 404 - ROTA
 app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header(
+        "Access-Control-Allow-Headers", 
+        "Origin, X-Requested-With, Content-Type,Accept, x-client-key, x-client-token, x-client-secret, Authorization");
     const err = new Error("Not Found");
     err.status = 404;
     next(err);
